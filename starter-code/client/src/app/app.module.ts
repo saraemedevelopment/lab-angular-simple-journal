de95714f9aca6def2c1f5adb8f7954beb40ca7d3
Sara <saraeme20@gmail.com>
2017-11-30 18:03:50 +0100

    iteration 3 done

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule } from '@angular/router';
+
+import { routes } from './routes';
+
 import { AppComponent } from './app.component';
+import { EntryListComponent } from './entry-list/entry-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot (routes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ starter-code/client/src/app/services/service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class CinemaService {
+  title: string;
+  date:string ;
+
+  constructor() { }
+
+   Array<Object> = [ ]
+
+
+}
